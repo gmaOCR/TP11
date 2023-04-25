4fdd3cd4ba9410703fee89b6f7f4fbc24078276c
Greg M <marainigregory@pm.me>
2023-04-25 09:57:00 +0200

    minor update

--- server.py
+++ server.py
@@ -28,7 +28,6 @@ def index():
 def showSummary():
     club = [club for club in clubs if club['email'] == request.form['email']][0]
     return render_template('welcome.html',club=club,competitions=competitions) """
-    # server.py
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
